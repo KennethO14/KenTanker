a116daef8eac6cdfb4d3734530a4cba21f648882
davidpobi <pobidavid@gmail.com>
2017-01-24 18:53:28 +0000

    divide module re-implemented

--- GitTest.java
+++ GitTest.java
@@ -22,6 +22,12 @@ public class GitTest {
             System.out.println(first * second);
     }
     
+
+      if(args[0].equals ("divide")){
+      double first = double.parseDouble(args[1]);
+            double second = double.parseDouble(args[2]);
+            System.out.println(first / second);
+    }
     
   }
   
